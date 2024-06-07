3fd900f747f2295a960727116202d0f028c028a0
ObiFlo1 <rubflores1000@gmail.com>
2024-06-07 02:50:39 -0700

    added media queries

--- src/components /NavBar/Navbar.jsx
+++ src/components /NavBar/Navbar.jsx
@@ -11,15 +11,12 @@ function Navbar() {
   }
   return (
     <>
-      <nav>
+      <nav className="nav-bar">
         <div href="#navbar" className="logo">
           Ruben Flores
         </div>
 
         <ul className="nav-links">
-          <li>
-            <a href="#home">Home</a>{" "}
-          </li>
           <li>
             <a href="#about">About</a>
           </li>
