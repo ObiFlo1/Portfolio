14187168e5e8776bc0d0965b4d3ffa8c93eaa00c
ObiFlo1 <rubflores1000@gmail.com>
2024-06-07 12:12:53 -0700

    took out projects for now

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import LandingPage from "./components /LandingPage/LandingPage";
 import NavBar from "./components /NavBar/Navbar";
 import About from "./components /About/About.jsx";
-import Projects from "./components /Projects /Projects.jsx";
+
 import "./App.css";
 import Contact from "./components /Contact/Contact.jsx";
 import Footer from "./components /Footer/Footer.jsx";
@@ -12,7 +12,7 @@ function App() {
       <NavBar />
       <LandingPage />
       <About />
-      {/* <Projects /> */}
+
       <Contact />
       <Footer />
     </>
