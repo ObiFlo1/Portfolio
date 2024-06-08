6ad51eb17e277d0988798c603f9d2d2a1a6fd9d5
ObiFlo1 <rubflores1000@gmail.com>
2024-06-08 08:30:15 -0700

    added back end server to handle form submissions

--- src/components /Contact/Contact.jsx
+++ src/components /Contact/Contact.jsx
@@ -1,10 +1,40 @@
 import "./Contact.css";
+import { useState } from "react";
+import axios from "axios";
 
 function Contact() {
+  // Form Data Submission
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post(
+        "https://rubenfloresportfolio.netlify.app/#contact",
+        formData || "https://rubenfloresportfolio.netlify.app/",
+        formData
+      )
+      .then((response) => {
+        alert("Form submitted successfully");
+      })
+      .catch((error) => {
+        alert("Error submitting form");
+      });
+  };
+
   return (
     <section id="contact" className="contact-container">
       <h1>Contanct</h1>
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <div className="formGroup">
           <label htmlFor="name" hidden>
             Name
@@ -15,6 +45,8 @@ function Contact() {
             id="name"
             placeholder="Name"
             required
+            value={formData.name}
+            onChange={handleChange}
           />
         </div>
         <div className="formGroup">
@@ -27,6 +59,8 @@ function Contact() {
             id="email"
             placeholder="email"
             required
+            value={formData.email}
+            onChange={handleChange}
           />
         </div>
         <div className="formGroup">
@@ -39,6 +73,8 @@ function Contact() {
             id="message"
             placeholder="message"
             required
+            value={formData.message}
+            onChange={handleChange}
           ></textarea>
         </div>
         <input className="hover btn" type="submit" value="Submit" />
---
+++ src/serverAPI/serverAPI.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
+const cors = require("cors");
+const app = express();
+const port = 7001;
+require("dotenv").config();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const webURL = " https://rubenfloresportfolio.netlify.app/";
+const myEmail = process.env.EMAIL;
+const myPassword = process.env.PASSWORD;
+
+//setting up email we want to send the client's contact information to
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: myEmail,
+    pass: myPassword,
+  },
+});
+
+app.post("/#contact", (req, res) => {
+  const { name, email, message } = req.body;
+
+  const mailOptions = {
+    from: email,
+    to: myEmail,
+    subject: `Contact form submission from ${email}`,
+    text: message,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return res.status(500).send("Error sending email");
+    }
+    res.status(200).send("Email sent successfully");
+  });
+});
+
+// port
+app.listen(port, () => {
+  console.log(`server listening on port: ${port}`);
+});
