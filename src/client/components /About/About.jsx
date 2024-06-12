bcf079629e32fc6a11a305060ca2259d8dd6799e
ObiFlo1 <rubflores1000@gmail.com>
2024-06-12 13:57:20 -0700

    added import media query from react

--- src/client/components /About/About.jsx
+++ src/client/components /About/About.jsx
@@ -1,29 +1,49 @@
 import "./About.css";
+import React from "react";
+import { useMediaQuery } from "react-responsive";
 
 function About() {
+  const isMobile = useMediaQuery({ maxWidth: 392 });
+
   return (
     <section id="about">
       <div className="about-container">
         <h1>About</h1>
+
         <p>
-          Hello! I'm Ruben Flores, a recent graduate of the Galvanize Level Up
-          by Hack Reactor coding bootcamp, specializing in PERN stack
-          development. With a solid foundation in PostgreSQL, Express.js, React,
-          and Node.js, I am actively working on various projects to deepen my
-          expertise and showcase my skills. Before transitioning to tech, I
-          served in the United States Army for six years as a Sergeant and
-          Combat Engineer (E-5). My experience in the military honed my
-          leadership, problem-solving abilities, and work ethic. Leading a team
-          of soldiers in high-pressure environments taught me the importance of
-          collaboration, discipline, and resilience—qualities I bring to my
-          development work. I am passionate about building robust and scalable
-          web applications and continuously learning new technologies. My goal
-          is to secure a role in a dynamic and innovative company where I can
-          contribute to exciting projects and grow as a developer. In my
-          personal life, I enjoy lifting weights and training my dog, which
-          helps me maintain a balanced and focused mindset. I am eager to bring
-          my unique background and skill set to a forward-thinking team and make
-          a meaningful impact in the tech industry."
+          {isMobile ? (
+            <>
+              Hello! I'm Ruben Flores, a recent graduate of the Galvanize Level
+              Up by Hack Reactor coding bootcamp, specializing in PERN stack
+              development. With a solid foundation in PostgreSQL, Express.js,
+              React, and Node.js, I am actively working on various projects to
+              deepen my expertise and showcase my skills. Before transitioning
+              to tech, I served in the United States Army for six years as a
+              Sergeant and Combat Engineer (E-5).
+            </>
+          ) : (
+            <>
+              Hello! I'm Ruben Flores, a recent graduate of the Galvanize Level
+              Up by Hack Reactor coding bootcamp, specializing in PERN stack
+              development. With a solid foundation in PostgreSQL, Express.js,
+              React, and Node.js, I am actively working on various projects to
+              deepen my expertise and showcase my skills. Before transitioning
+              to tech, I served in the United States Army for six years as a
+              Sergeant and Combat Engineer (E-5). My experience in the military
+              honed my leadership, problem-solving abilities, and work ethic.
+              Leading a team of soldiers in high-pressure environments taught me
+              the importance of collaboration, discipline, and
+              resilience—qualities I bring to my development work. I am
+              passionate about building robust and scalable web applications and
+              continuously learning new technologies. My goal is to secure a
+              role in a dynamic and innovative company where I can contribute to
+              exciting projects and grow as a developer. In my personal life, I
+              enjoy lifting weights and training my dog, which helps me maintain
+              a balanced and focused mindset. I am eager to bring my unique
+              background and skill set to a forward-thinking team and make a
+              meaningful impact in the tech industry."
+            </>
+          )}
         </p>
       </div>
     </section>
